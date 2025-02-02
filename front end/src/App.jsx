6b390e6e5b84ecfa53c577bda9e05cb8bd0f7259
AnubhavRegmi <regmia@warhawks.ulm.edu>
2025-02-02 00:27:29 -0600

    footer and routes setup complete

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import Home from "./Pages/Home";
+import Dogs from "./Pages/Dogs"; 
+import Adoption from "./Pages/Adoption";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <div>
-
-      <Navbar />
-    
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex-grow container mx-auto mt-4 px-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dogs" element={<Dogs />} />
+            <Route path="/adoption" element={<Adoption />}/>
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
 export default App;
+
