499f37b825f9c3b5e2ab1aefc633d37e2a7f0cdd
chakrabortyrohan74 <chakrabortyrohan74@gmail.com>
2025-02-03 02:38:27 -0600

    changed port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');const { MongoClient, ServerApiVersion, ObjectId } =
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 const uri = process.env.uri;
 
