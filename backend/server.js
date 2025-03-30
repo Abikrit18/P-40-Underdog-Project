d2719bce75a629e994a0ef486a439e60ee64bb98
Abikrit <abikritgautam123@gmail.com>
2025-03-30 17:22:18 -0500

    changed backend port for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const walkRoutes = require('./routes/walkRoutes');
 const shelterTimeRoutes = require('./routes/shelterTimeRoutes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
