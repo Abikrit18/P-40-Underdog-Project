19fd2ebaf063ea80d09eb025edb44405d6c68232
Joey Roubique <joeyroubique@yahoo.com>
2025-05-06 17:28:11 -0500

    Changed Complete Walk on Profile to Check In

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -390,7 +390,7 @@ const Profile = () => {
                                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                                                         </svg>
-                                                        Complete Walk
+                                                        Check In
                                                     </button>
                                                     <button
                                                         onClick={() => handleDidNotShow(walk._id)}
