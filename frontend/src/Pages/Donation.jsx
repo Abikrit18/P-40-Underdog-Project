877130fe86161333ff440bdb57db0cdf7a1631cf
Joey Roubique <joeyroubique@yahoo.com>
2025-05-06 17:14:32 -0500

    Broke Donation 💣

--- frontend/src/Pages/Donation.jsx
+++ frontend/src/Pages/Donation.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 const Donation = () => {
   return (
-    <div className="h-screen flex justify-center items-center bg-gray-100">
+    <div className="w-full h-screen bg-gray-100 flex justify-center items-center">
       <iframe
         src="https://www.clover.com/pay-widgets/d682cd3a-15d8-4ba1-8bfb-b0d73eeb867b"
-        title="Donation Page"
-        className="w-full h-full border-none"
-      ></iframe>
+        title="Clover Donation Widget"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+        }}
+      />
     </div>
   );
 };
